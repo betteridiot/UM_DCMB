4986b07ebca53e3802d0481d4bdc0ab0ad707ae4
mdsherm <mdsherm@users.noreply.github.com>
2016-03-14 03:22:24 -0400

    updated

--- Mills_Lab/modulo_check.py
+++ Mills_Lab/modulo_check.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
+import os
+import re
 
 
 def modulo_check(NUM):
@@ -21,12 +23,22 @@ def modulo_check(NUM):
 
 
 def main():
-    num = int(sys.argv[1])
+    pat1 = re.compile(r'unannotatedchr\d{1,2}')
+    pat2 = re.compile(r'/home.*/chr\d{1,2}/')
+    infile = sys.argv[1]
+    output = pat1.search(infile).group()
+    path = pat2.search(infile).group()
+    line_count = os.popen("gzip -cd %s | wc -l" % infile)
+    num = int((line_count.next()).rstrip())
     div = modulo_check(num)
     out = num/div
+    os.popen("gzip -cd %s | split -l %d  - %s" % (infile, out, output))
     print("Greatest Divisor: %d" % div)
     print("Lines per file: %d" % out)
-
+    os.chdir(path)
+    os.popen("find . -type f -exec mv '{}' '{}'.vcf \;")
+    os.popen("rm -f *.gz.vcf")
+    os.popen('for f in *.vcf; do   bgzip "$f"; done')
 
 if __name__ == '__main__':
     main()
