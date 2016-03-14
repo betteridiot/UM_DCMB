c5ce2126c79d6f93c0e48bfd9355730ec8addfbe
mdsherm <mdsherm@umich.edu>
2016-03-14 06:53:01 -0400

    Put the bamlist maker back in. The output files weren't containing anything

--- Mills_Lab/ORFSNuPer.py
+++ Mills_Lab/ORFSNuPer.py
@@ -77,13 +77,50 @@ plusStart = ("TAC",)
 negStart = ("CAT",)
 # CODONS END
 
+# Adding in ribosome sample name to SRA ID file
+ribosamples = []
+with open('/home/mdsherm/Project/ribosamplescross') as ribo:
+    reader = csv.reader(ribo, delimiter='\t')
+    ribosamples.extend([row for row in reader])
 
-list_files = ["RNAsamp_crossref", "Ribosamp_crossref", "RNAbams_dict", "Ribobams_dict"]
-bamDir = "/home/mdsherm/Project/UM_DCMB/Mills_Lab/pkl/"
-for name in list_files:
-    with open('%s%s.pkl' % (bamDir, name), 'rb') as f:
-        n = name
-        globals()[n] = pickle.load(f)
+# makes lists of all RNA-seq and ribosome profiling BAM files
+RNAbams, Ribobams = [], []
+RNAbams_total, Ribobams_total = [], []
+# dir, _, _ implies dir, dirs, files...however, I only want the top object
+os.chdir(RNADir)
+for dir, _, _ in os.walk(os.getcwd()):
+    RNAbams.extend(glob.glob(os.path.join(dir, "*hits.bam")))
+os.chdir(riboDir)
+for dir, _, _ in os.walk(os.getcwd()):
+    Ribobams.extend(glob.glob(os.path.join(dir, "*sort.bam")))
+with open("/home/mdsherm/Project/RNAsamples", 'r') as rnasamples:
+    samplenames = rnasamples.read().splitlines()
+RNAsamp_crossref = [(name, [entry for entry in RNAbams if name in entry]) for name in samplenames]
+Ribosamp_crossref = [(name[1], [entry for entry in Ribobams if name[0] in entry]) for name in ribosamples]
+
+
+# Make a dict of Samples and FPKM
+def read_indexer(fileDIR, LIST):
+    for item in fileDIR:
+        fullcounter = os.popen("samtools idxstats " + item + " |awk '{sum+=$3} END {print sum}'")
+        fullcount = float(fullcounter.readline().rstrip())
+        LIST.extend([[item, fullcount]])
+    return {key: value for (key, value) in LIST}
+
+
+# Matches sample names to bam files and pulls out total reads of
+RNAbams_dict = read_indexer(RNAbams, RNAbams_total)
+RNAtotalreads = dict(zip(samplenames, [sum([RNAbams_dict.get(j) for j in i[1]])for i in RNAsamp_crossref]))
+Ribobams_dict = read_indexer(Ribobams, Ribobams_total)
+Ribototalreads = dict(zip(samplenames, Ribobams_dict.values()))
+
+
+# list_files = ["RNAsamp_crossref", "Ribosamp_crossref", "RNAbams_dict", "Ribobams_dict"]
+# bamDir = "/home/mdsherm/Project/UM_DCMB/Mills_Lab/pkl/"
+# for name in list_files:
+#     with open('%s%s.pkl' % (bamDir, name), 'rb') as f:
+#         n = name
+#         globals()[n] = pickle.load(f)
 
 
 # Used to iterate potential ORF class instantiation
