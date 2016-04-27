0df756de0a8fea4c585ebc94e8ec66874187db4c
mdsherm <mdsherm@umich.edu>
2016-04-27 11:17:47 -0400

    Cleaning up argparse

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -239,11 +239,11 @@ def main():
     global sampleGroup
     global SNPs
     parser = argparse.ArgumentParser(description='Plots relevant SNPs to interactive scatterplot')
-    parser.add_argument('-d', action='store', dest='dir', help='root directory of interest',
-                        default='/home/mdsherm/Project/SNuPer_results')
-    parser.add_argument('--rna', action='store', dest='rna', type=float,
+    parser.add_argument('-d', action='store', dest='dir', help='/path/to/root/dir',
+                        metavar="dir", default='/home/mdsherm/Project/SNuPer_results')
+    parser.add_argument('--rna', action='store', dest='rna', type=float, metavar="float",
                         help='threshold for %% of RNA-seq FPKM', default=.5)
-    parser.add_argument('--ribo', action='store', dest='ribo', type=float,
+    parser.add_argument('--ribo', action='store', dest='ribo', type=float, metavar="float",
                         help='threshold for %% of ribosomal profiling FPKM', default=.2)
     parser.add_argument('-t', action='store', dest='top', type=int,
                         help='The number of top results based on log2 ratio of'
