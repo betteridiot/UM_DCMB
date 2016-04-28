697860286afda61881b6bfe58267efe54b18929a
mdsherm <mdsherm@umich.edu>
2016-04-28 13:28:16 -0400

    Trying out whole SNPs file

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -319,7 +319,8 @@ def main():
         print("No precompiled list present!")
         print("Exiting")
     elif args.plot and os.path.isfile(path_name + '/pkl/top_%d.pkl' % args.top):
-            top = pickle.load(open(path_name + '/pkl/top_%d.pkl' % args.top))
+            # top = pickle.load(open(path_name + '/pkl/top_%d.pkl' % args.top))
+            top = pickle.load(open(path_name + '/pkl/plotzip.pkl'))
             SNPs = pickle.load(open(path_name + "/pkl/SNPs.pkl", "rb"))
             # sampleGroup = pickle.load(open(path_name + "/pkl/SG.pkl", "rb"))
             qLook = {entry[0].split(".snp")[0]: i for (i, entry) in enumerate(SNPs)}
