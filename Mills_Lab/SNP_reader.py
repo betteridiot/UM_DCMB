42104006f0c9790e4f6407f50dfa9185ff7ccc9a
mdsherm <mdsherm@umich.edu>
2016-04-28 13:53:08 -0400

    Trying out whole SNPs file

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -334,12 +334,12 @@ def main():
                 min(colors),4), round(max(colors),4)))
             x = [snp[0] for snp in percents]
             y = [snp[1] for snp in percents]
-            # z = [1 * (len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
-            #                  len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))/
-            #            len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]) for snp in top]
-            z = [.2 - ((len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
-                      len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))**2)**.5 for snp in
-                 top]
+            z = [.2 - (((len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
+                             len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))/
+                       len(SNPs[qLook.get(snp[0].split(".snp")[0])][5]))**2)**.5 for snp in top]
+            # z = [.2 - ((len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
+            #           len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))**2)**.5 for snp in
+            #      top]
 
             # Plots the points above, and can be used to tie in individual SNP IDs
             fig, ax = plt.subplots()
