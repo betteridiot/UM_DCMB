2cbd2b1f213698043f9f288eec43736f0c342c54
mdsherm <mdsherm@umich.edu>
2016-04-28 13:50:43 -0400

    Trying out whole SNPs file

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -334,10 +334,10 @@ def main():
                 min(colors),4), round(max(colors),4)))
             x = [snp[0] for snp in percents]
             y = [snp[1] for snp in percents]
-            z = [1 * (len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
-                             len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))/
-                       len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]) for snp in top]
-            z = [1 - ((len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
+            # z = [1 * (len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
+            #                  len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))/
+            #            len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]) for snp in top]
+            z = [.2 - ((len(SNPs[qLook.get(snp[0].split(".snp")[0])][4]) -
                       len(SNPs[qLook.get(snp[0].split(".snp")[0])][2]))**2)**.5 for snp in
                  top]
 
