2e927834f971f92364a43ac8767bf098cfe5ee3c
mdsherm <mdsherm@umich.edu>
2016-04-28 13:54:13 -0400

    Trying out whole SNPs file

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -321,7 +321,7 @@ def main():
     elif args.plot and os.path.isfile(path_name + '/pkl/top_%d.pkl' % args.top):
             # top = pickle.load(open(path_name + '/pkl/top_%d.pkl' % args.top))
             top = pickle.load(open(path_name + '/pkl/plotzip.pkl'))
-            top = [snp for snp in top if snp[2] > 1]
+            top = [snp for snp in top if snp[2] > 1][:100]
             SNPs = pickle.load(open(path_name + "/pkl/SNPs.pkl", "rb"))
             # sampleGroup = pickle.load(open(path_name + "/pkl/SG.pkl", "rb"))
             qLook = {entry[0].split(".snp")[0]: i for (i, entry) in enumerate(SNPs)}
