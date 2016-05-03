95900e489bf523ec9be87abae216465830572ef5
mdsherm <mdsherm@umich.edu>
2016-05-03 13:40:17 -0400

    Fixing the median error for handling of inf

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -196,16 +196,14 @@ class AnnoteFinder(object):
             ylab = "FPKM"
             title = string + '/n' + 'log2[alt/ref] = %f' % np.log2(np.mean(homoribo)/np.mean(refribo))
             figmix, (axrna, axribo, axnorm) = plt.subplots(1,3)
-            axrna.boxplot(rna, labels=ticks, notch=True, whis=[5, 95],
+            axrna.boxplot(rna, labels=ticks,  whis=(5, 95),
                           usermedians=rna_median)
             axrna.set_title(title + ": RNA-seq (N=%d)"
                             % sum((len(hetrna), len(homorna), len(refrna))),
                             fontsize=8)
             axrna.set_ylabel(ylab, fontsize=8)
             axrna.set_xlabel(xlab, fontsize=8)
-            plt.xticks(fontsize=8)
-            plt.yticks(fontsize=8)
-            axribo.boxplot(ribo, labels=ticks, notch=True, whis=[5, 95],
+            axribo.boxplot(ribo, labels=ticks, whis=(5, 95),
                            usermedians=ribo_median)
             axribo.set_title(title + ": Ribosome Profiling (N=%d)"
                             % sum((len(hetrna), len(homorna), len(refrna))),
@@ -216,20 +214,17 @@ class AnnoteFinder(object):
                              # fontsize=8)
             axribo.set_ylabel(ylab, fontsize=8)
             axribo.set_xlabel(xlab, fontsize=8)
-            plt.xticks(fontsize=8)
-            plt.yticks(fontsize=8)
-            axnorm.boxplot(norm, labels=ticks, notch=True, whis=[5, 95],
+            axnorm.boxplot(norm, labels=ticks, whis=(5, 95),
                            usermedians=norm_median)
             axnorm.set_title(title + ": Normalized Ribo (N=%d)"
                             % sum((len(hetrna), len(homorna), len(refrna))),
                             fontsize=8)
             axnorm.set_ylabel(ylab, fontsize=8)
-            plt.xticks(fontsize=8)
-            plt.yticks(fontsize=8)
             axnorm.set_xlabel(xlab, fontsize=8)
             plt.suptitle('log2[alt/ref] = %f'
                              % np.log2(np.mean(homoribo)/np.mean(refribo)),
                              fontsize=8)
+            plt.tick_params(axis='both', labelsize=8)
             plt.tight_layout()
             figmix.show()
 
