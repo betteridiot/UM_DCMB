55e676cc70f6fee24b89bb0c4bd952beada488e8
mdsherm <mdsherm@umich.edu>
2016-05-04 12:36:57 -0400

    Changing up boxplots

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -251,6 +251,8 @@ class AnnoteFinder(object):
             figmix.suptitle(title, fontsize=8)
             plt.tick_params(axis='both', labelsize=8)
             plt.tight_layout()
+            figManager = plt.get_current_fig_manager()
+            figManager.window.showMaximized()
             figmix.show()
 
     def drawAnnote(self, ax, x, y, annote):
