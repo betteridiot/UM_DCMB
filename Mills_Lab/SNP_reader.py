66871056f093f75280a4a489d0f9ac4db2593cd5
mdsherm <mdsherm@umich.edu>
2016-05-04 14:09:12 -0400

    Figuring out window maximization for boxplot

--- Mills_Lab/SNP_reader.py
+++ Mills_Lab/SNP_reader.py
@@ -252,7 +252,7 @@ class AnnoteFinder(object):
             plt.tick_params(axis='both', labelsize=8)
             plt.tight_layout()
             figManager = plt.get_current_fig_manager()
-            figManager.window.showNormal()
+            figManager.window.showMaximized()
             figmix.show()
 
     def drawAnnote(self, ax, x, y, annote):
