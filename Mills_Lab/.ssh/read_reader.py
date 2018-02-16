c78e9af9bf5b7c620406b118d3295f34ab3ebbd4
better idiot <betteridiot@users.noreply.github.com>
2018-02-16 09:14:25 -0500

    Update read_reader.py

--- Mills_Lab/.ssh/read_reader.py
+++ Mills_Lab/.ssh/read_reader.py
@@ -22,20 +22,20 @@ def main():
         snp_chrom, snp_pos = snp_pos.split('_')
         region = "{}-{}".format(int(snp_pos)-1000, int(snp_pos)+1000)
 
-        reference = '/home/mdsherm/Project/Reference/hg19/Sequence/Chromosomes'
-        project_root = '/home/mdsherm/Project/SNuPer_results'
+        reference = '~/Project/Reference/hg19/Sequence/Chromosomes'
+        project_root = '~/Project/SNuPer_results'
         snp_file = os.popen('locate -d {}/chr{}/mlocate.db {}.snp'.format(
             project_root, snp_chrom, sys.argv[1])).read().rstrip()
         snp_geno = {row[0]:row[1] for row in csv.reader(open(snp_file, "rb"), delimiter='\t')}
 
-        rna_path = '/home/mdsherm/Rotation/RNA_fq/tophat_hg19'
+        rna_path = '~/Rotation/RNA_fq/tophat_hg19'
         rna_bams = catcher(rna_path, "accepted_hits.bam")
         rna_cross = {rna_bam:rna_bam.split('_')[2].split('/')[1] for rna_bam in rna_bams}
 
-        ribo_path = '/home/mdsherm/Rotation/ribosomal/bwa_alignment_hg19'
+        ribo_path = '~/Rotation/ribosomal/bwa_alignment_hg19'
         ribo_bams = catcher(ribo_path, "*_sort.bam")
         ribo_cross = {}
-        for row in open('/home/mdsherm/Project/ribosamplescross', 'rb'):
+        for row in open('~/Project/ribosamplescross', 'rb'):
             for ribo_bam in ribo_bams:
                 if ribo_bam.split('/')[6].split('_')[0] in row.split()[0]:
                     ribo_cross.update({ribo_bam:row.split()[1]})
@@ -69,10 +69,10 @@ def main():
             np.mean(mix_alt, axis=0)[1], np.std(mix_alt, axis=0)[1]))
 
 # TODO command for building locate DB:
-# updatedb -U /home/mdsherm/Project/SNuPer_results/chr10 -o /home/mdsherm/Project/SNuPer_results/chr10/mlocate.db --require-visibility 0
+# updatedb -U ~/Project/SNuPer_results/chr10 -o /home/mdsherm/Project/SNuPer_results/chr10/mlocate.db --require-visibility 0
 
 # TODO command to use locate:
-# locate -d /home/mdsherm/Project/SNuPer_results/chr10/mlocate.db 10_15429889:XY.snp
+# locate -d ~/Project/SNuPer_results/chr10/mlocate.db 10_15429889:XY.snp
 
 if __name__ == "__main__":
     main()
